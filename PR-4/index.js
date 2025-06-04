04109fe7e53b1d0e0a5bc011cc847d69058f064c
Jaydip Vora <jaydipvora9898@gmail.com>
2025-06-04 08:43:19 +0530

    change

--- PR-4/index.js
+++ PR-4/index.js
@@ -1,5 +1,158 @@
-function greet(name) {
-    console.log("Hello, " + name + "!");
-  }
-  greet("John");
-  
+// 1. Write a Javascript program to print a greeting Message using with argument no return type function without argument at that time Print message Good Morning
+
+// function greet(name) {
+//   console.log("Good Morning 🥂🥂");
+// }
+
+// greet();
+
+
+// Expected Output:
+
+// Good Morning 🥂🥂
+
+
+
+// 2. Write a Javascript program to Find circle area using return type  with argument function
+
+// function circleArea(r) {
+//   return 3.14 * r * r;
+// }
+
+// console.log(circleArea(5));
+
+// Expected Output:
+
+// 78.5
+
+
+
+
+// 3. Write a Javascript program to Find Triangle area using return type  with argument function
+
+// function TriangleArea(l,h) {
+
+//     return 0.5 * l * h;
+
+//   }
+
+// console.log(TriangleArea(5,30));
+
+
+// Expected Output:
+
+// 75
+
+
+// 4. Write a Javascript program to Find Rectangle area using return type  with argument function
+
+// function RectangleArea(l,h) {
+
+//       return l * h;
+//     }
+// console.log(RectangleArea(5,30));
+
+
+// Expected Output:
+
+// 150
+
+
+// 5. Write a Javascript program to Find Ans of ((b*b)(4*a*c))/(2*a) using return type  with argument function
+
+// function quadratic(a, b, c) {
+
+//   return ((b*b) - (4*a*c))/(2*a);
+// }
+
+// console.log(quadratic(1, 2, 3));
+
+
+// Expected Output:
+
+// -4
+
+
+// 6. Write a Javascript program to Find Ans of (a*a) + (2*a*b) + (b*b) using return type  with argument function
+
+
+// function quadratic(a, b) {
+
+//     return (a*a) + (2*a*b) + (b*b);
+
+// }
+
+// console.log(quadratic(5, 23));
+
+
+// Expected Output:
+
+// 784
+
+
+
+// 7. Write a Javascript program to Convert Fahrenheit to Celsius using return type  with argument function
+
+// function Celsius(fahrenheit) {
+
+//     return (fahrenheit - 32) * 5/9;
+// }
+
+// console.log(Celsius(100));
+
+// Expected Output:
+
+// 37.77777777777778
+
+
+
+// 8. Write a Javascript program to Convert Celsius to Fahrenheit using return type  with argument function
+
+// function Fahrenheit(celsius) {
+
+//     return (celsius * 1.8) + 32;
+// }
+
+// console.log(Fahrenheit(100));
+
+
+// Expected Output:
+
+// 212
+
+
+// 9. Write a Javascript program to find if a given number is odd or Even using an argument with no return type.
+
+// function evenOdd(num) {
+
+//     if(num % 2 === 0) {
+//         console.log("Even");
+//     } else {
+//         console.log("Odd");
+//     }
+// }
+// evenOdd(6);
+
+
+// Expected Output:
+
+// Even
+
+
+
+// 10.Write a Javascript program to swap a value Without third variable using With argument No return type
+
+// function swapValues(a, b) {
+//   console.log("Before Swap: a = ", a, ", b = ", b);
+//   a = a + b;
+//   b = a - b;
+//   a = a - b;
+//   console.log("After Swap: a = ", a, ", b = ", b);
+// }
+// swapValues(5, 10);
+
+// Expected Output:
+
+// Before Swap: a =  5 , b =  10
+// After Swap: a =  10 , b =  5
+
